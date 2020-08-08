8e8196e6c12ffac85179446bdd1b01e399d591e7
Shreyz-max <65608549+Shreyz-max@users.noreply.github.com>
2020-08-08 10:35:32 +0530

    Update assignment 3.py

--- assignment 3.py
+++ assignment 3.py
@@ -1,3 +1,7 @@
+# Question 1.1
+# 1.1 Write a Python Program to implement your own myreduce() function which works exactly
+# like Python's built-in function reduce()
+
 # myreduce function
 def myreduce(func, lists):
     x = lists[0]
@@ -12,6 +16,10 @@ def add(x1, x2):
 
 ans = myreduce(add, [1, 2, 3, 4])
 print(ans)
+
+# Question 1.2
+#1.2 Write a Python program to implement your own myfilter() function which works exactly
+#like Python's built-in function filter()
 
 # myfilter function
 def myfilter(func, sequence):
@@ -33,6 +41,14 @@ adults = filter(myFunc, [22, 13, 21])
 print(adults)
 print(myfilter(myFunc, [22, 13, 21]))
 
+# Question 2
+# Implement List comprehensions to produce the following lists.
+# Write List comprehensions to produce the following Lists
+# ['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+# ['x', 'y', 'z', 'xx', 'yy', 'zz', 'xxx', 'yyy', 'zzz', 'xxxx', 'yyyy', 'zzzz']
+# [[2], [3], [4], [3], [4], [5], [4], [5], [6]] [[2, 3, 4, 5], [3, 4, 5, 6],
+# [4, 5, 6, 7], [5, 6, 7, 8]]
+# [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
 # list comprehensions
 seq = [a * i for a in ['x', 'y', 'z'] for i in range(1, 5)]
 print(seq)
