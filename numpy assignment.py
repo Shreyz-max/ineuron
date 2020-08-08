58cc06a3e200cae64f7c41e693da826623e162fa
Shreyz-max <65608549+Shreyz-max@users.noreply.github.com>
2020-08-08 10:57:36 +0530

    Update numpy assignment.py

--- numpy assignment.py
+++ numpy assignment.py
@@ -1,3 +1,12 @@
+# Question 1
+# Write a function so that the columns of the output matrix are powers of the input
+# vector.
+# The order of the powers is determined by the increasing boolean argument. Specifically, when
+# increasing is False, the i-th output column is the input vector raised element-wise to the power
+# of N - i - 1.
+
+# HINT: Such a matrix with a geometric progression in each row is named for Alexandre-
+# Theophile Vandermonde.
 import numpy as np
 
 x = np.array([1, 2, 3, 4, 5])
@@ -7,7 +16,22 @@ print(x**n)
 arr = []
 newArray = np.column_stack([x**(n-i-1) for i in range(n)])
 
-
+# Problem Statement 2:
+# Given a sequence of n values x1, x2, ..., xn and a window size k>0, the k-th moving average of
+# the given sequence is defined as follows:
+# The moving average sequence has n-k+1 elements as shown below.
+# The moving averages with k=4 of a ten-value sequence (n=10) is shown below
+# i 1 2 3 4 5 6 7 8 9 10
+# ===== == == == == == == == == == ==
+# Input 10 20 30 40 50 60 70 80 90 100
+# y1 25 = (10+20+30+40)/4
+# y2 35 = (20+30+40+50)/4
+# y3 45 = (30+40+50+60)/4
+# y4 55 = (40+50+60+70)/4
+# y5 65 = (50+60+70+80)/4
+# y6 75 = (60+70+80+90)/4
+# y7 85 = (70+80+90+100)/4
+# Thus, the moving average sequence has n-k+1=10-4+1=7 values.
 def moving_window(seq, k):
     final = []
     for i in range(len(seq)-k+1):
