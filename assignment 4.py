707c97372538c9fb6961e0e5d460e9e2322c3d79
Shreyz-max <65608549+Shreyz-max@users.noreply.github.com>
2020-08-08 10:42:02 +0530

    Update assignment 4.py

--- assignment 4.py
+++ assignment 4.py
@@ -1,3 +1,11 @@
+# Question 1.1
+# 1.1 Write a Python Program(with class concepts) to find the area of the triangle using the below
+# formula.
+# area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+# Function to take the length of the sides of triangle from user should be defined in the parent
+# class and function to calculate the area should be defined in subclass.
+
 class Triangle:
     def __init__(self):
         self.a = 0
@@ -27,6 +35,9 @@ t = Area()
 t.input_sides()
 print(t.get_area())
 
+# Question 1.2
+# 1.2 Write a function filter_long_words() that takes a list of words and an integer n and returns
+# the list of words that are longer than n.
 
 def filter_long_words(words, n):
     x = []
@@ -35,10 +46,18 @@ def filter_long_words(words, n):
             x.append(word)
     return x
 
+# Question 2.1
+# 2.1 Write a Python program using function concept that maps list of words into a list of integers
+# representing the lengths of the corresponding words.
+# Hint: If a list [ ab,cde,erty] is passed on to the python function output should come as [2,3,4]
+# Here 2,3 and 4 are the lengths of the words in the list.
 
 seq = list(map(lambda x: len(x), ['ab', 'cde', 'erty']))
 print(seq)
 
+# Question 2.2
+# 2.2 Write a Python function which takes a character (i.e. a string of length 1) and returns True if
+# it is a vowel, False otherwise.
 
 def vowel_or_not(letter):
     vowel = ['a', 'e', 'i', 'o', 'u']
