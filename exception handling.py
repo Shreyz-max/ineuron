14616bdbba1be8a8b52fe4c36ba01ab15f819362
Shreyz-max <65608549+Shreyz-max@users.noreply.github.com>
2020-08-08 10:45:19 +0530

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -1,7 +1,28 @@
+# Question 1
+# 1. Write a function to compute 5/0 and use try/except to catch the exceptions.
 try:
     print(5/0)
 except:
     print('Error dividing by zero')
+    
+# Question 2
+# 2. Implement a Python program to generate all sentences where subject is in
+# ["Americans", "Indians"] and verb is in ["Play", "watch"] and the object is in
+# ["Baseball","cricket"].
+# Hint: Subject,Verb and Object should be declared in the program as shown below.
+# subjects=["Americans ","Indians"]
+# verbs=["play","watch"]
+# objects=["Baseball","Cricket"]
+
+# Output should come as below:
+# Americans play Baseball.
+# Americans play Cricket.
+# Americans watch Baseball.
+# Americans watch Cricket.
+# Indians play Baseball.
+# Indians play Cricket.
+# Indians watch Baseball.
+# Indians watch Cricket.
 
 subjects = ["Americans ", "Indians "]
 verbs = ["play ", "watch "]
